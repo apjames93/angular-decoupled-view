a1aed63f3a007e82436efcfed66501e96c5609c3
Alexander James <AlexJames@Alexanders-MacBook-Air.local>
2016-08-06 13:29:38 -0600

    fixs bugs and added in first home view

--- js/app.js
+++ js/app.js
@@ -2,4 +2,10 @@ var app = angular.module("todo-angular", ["ui.router"]);
 
 app.config(function($stateProvider, $urlRouterProvider, $locationProvider){
   $urlRouterProvider.otherwise("/");
+
+  $stateProvider
+    .state("home", {
+      url: "/",
+      templateUrl: "/templates/home.html"
+    });
 });

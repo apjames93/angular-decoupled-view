d5b0e573c3a04fd863a37d943dfea350d8096466
Alexander James <AlexJames@Alexanders-MacBook-Air.local>
2016-08-06 14:34:28 -0600

    set up directive for signup

---
+++ components/signup/signup-directive.js
@@ -0,0 +1,22 @@
+angular
+  .module('todo-angular.signup.signup-directive', [])
+  .directive('signup', signup);
+
+function signup(){
+    var directive = {
+// restrict: "E " is restricting it to a html element
+      restrict: 'E',
+      templateUrl: 'templates/signup.html',
+// set up the controller. controllerAs: is assinging a name to the controller for when the file is minified
+      controller: signupController,
+      controllerAs: 'signupController'
+    }
+
+    return directive;
+}
+
+signupController.$inject = ['$scope'];
+
+function signupController($scope) {
+  $scope.toots = 'smells';
+}
